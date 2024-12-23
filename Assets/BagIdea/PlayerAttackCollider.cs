dcd0b3bbaeb5120fdbec61c13279cbfd3b111414
84RCH <yajirushi0513@gmail.com>
2024-12-23 15:34:04 +0900

    ステージ設計中

--- Assets/BagIdea/PlayerAttackCollider.cs
+++ Assets/BagIdea/PlayerAttackCollider.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class PlayerAttackCollider : MonoBehaviour
 {
     [SerializeField] float damage =1f;
+    [SerializeField] GameObject hitEffectPrefab; // パーティクルシステムのPrefab
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.layer);
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             Debug.Log("Hit Enemy");
+
+            // ダメージ処理
             DamageSystem playerDamageSystem = other.GetComponent<DamageSystem>();
-            playerDamageSystem.TakeDamage(damage);
+            if (playerDamageSystem != null)
+            {
+                playerDamageSystem.TakeDamage(damage);
+            }
+
+            // エフェクト再生
+            PlayHitEffect(other.ClosestPoint(transform.position));
         }
-     
+    }
+
+    private void PlayHitEffect(Vector3 position)
+    {
+        if (hitEffectPrefab != null)
+        {
+            // パーティクルシステムをインスタンス化して再生
+            Instantiate(hitEffectPrefab, position, Quaternion.identity);
+        }
     }
 }
